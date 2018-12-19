d8ce0fb581973c337ce4cb2168ee4cd449d0f71c
chen <beiowolf007@16.com>
2018-12-19 15:57:27 +0800

    autocoder v0.1

--- autocode.py
+++ autocode.py
@@ -1,6 +1,6 @@
 from keras.layers import Input,Dense
 from keras.models import Model
-import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
 #prepare data
@@ -12,7 +12,7 @@ print(x[:4])
 
 
 #make encoder
-input_data=Input((,6))
+input_data=Input(shape=(6,))
 encoded=Dense(256,activation='relu')(input_data)
 encoded=Dense(128,activation='relu')(encoded)
 encoded=Dense(64,activation='relu')(encoded)
@@ -23,6 +23,22 @@ encoded_data=Dense(2)(encoded)
 decoded=Dense(32,activation='relu')(encoded_data)
 decoded=Dense(64,activation='relu')(decoded)
 decoded=Dense(128,activation='relu')(decoded)
-decoded_data=Dense(256,activation='relu')(decoded)
+decoded=Dense(256,activation='relu')(decoded)
+decoded_data=Dense(6,)(decoded)
 
 #make model
+autoencoder=Model(inputs=input_data,outputs=decoded_data)
+
+#make encoder
+encoder_Model = Model(inputs=input_data,outputs=encoded_data)
+
+#compile and train
+autoencoder.compile(optimizer='Adam',loss='mse')
+autoencoder.fit(x,x,batch_size=5,epochs=10,shuffle=True)
+
+#to cluster
+cluster_data=encoder_Model.predict(x)
+print(cluster_data[0:3,])
+plt.scatter(cluster_data[:,0],cluster_data[:,1])
+plt.show()
+
