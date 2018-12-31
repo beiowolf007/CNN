1cd2a3a78064f494f94f5cac53fd7af4b4b55cd5
beiowolf007 <beiowolf007@163.com>
2018-12-31 18:09:20 +0800

    autocoder_v0.114

--- autocode.py
+++ autocode.py
@@ -6,8 +6,11 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.cluster import AgglomerativeClustering
 
+def preparer_data(data):
+    
+
 #prepare data
-tb_path=r'F:\chenyw\毕业论文\毕业\data\gz_con.xlsx'
+tb_path=r'F:\work\毕业论文\毕业\data\gz_con.xlsx'
 tb= pd.read_excel(tb_path ,'Sheet1')
 tb2=tb.iloc[:,2:]
 tb2=tb2.apply(lambda x:(x-np.min(x))/(np.max(x)-np.min(x)))
@@ -37,13 +40,28 @@ encoder_Model = Model(inputs=input_data,outputs=encoded_data)
 
 #compile and train
 autoencoder.compile(optimizer='Adam',loss='mse')
-autoencoder.fit(x,x,batch_size=15,epochs=30,shuffle=True)
+autoencoder.fit(x,x,batch_size=15,epochs=3,shuffle=True)
 
-#to cluster
+#to encode
 cluster_data=encoder_Model.predict(x)
 end_data=autoencoder.predict(x)
 print(end_data[:3,])
 print(cluster_data[0:3,])
-cluster_alg=AgglomerativeClustering(n_clusters=4)
-cluster_alg.fit(cluster_data)
-print(cluster_data[:3,])
+
+#cluster and show up
+plot_num=221
+mark_dic={0:'o',1:'v',2:'*',3:'p'}
+for linkage in ('ward', 'average', 'complete', 'single'):
+    cluster_alg=AgglomerativeClustering(n_clusters=4,linkage=linkage)
+    clusted=cluster_alg.fit(cluster_data)
+    trans_label=list()
+    for i in clusted.labels_:
+        trans_label.append(mark_dic[i])
+        
+    plt.subplot(plot_num)
+    plt.scatter(cluster_data[:,0],cluster_data[:,1],marker=trans_label)
+    my_title="%s linkage"%linkage
+    plt.title("%s linkage"%linkage)
+    plot_num=plot_num+1
+plt.show()
+
