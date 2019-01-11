78ee00ab2a81b33f6075c065d22b421cba952a30
chen <beiowolf007@163.com>
2019-01-12 00:29:26 +0800

    autocoder v0.21

--- gz_pca.py
+++ gz_pca.py
@@ -3,10 +3,13 @@ from sklearn.decomposition import PCA
 import pandas as pd
 
 tb_path=r'F:\chenyw\毕业论文\毕业\data\gz_con.xlsx'
-tb= pd.read_excel(tb_path ,'Sheet1')
-x=tb.iloc[:,1:].values
-
-my_pca=PCA(n_components='mle',svd_solver='full')
-my_pca.fit(x)
-print(my_pca.components_)
-print(my_pca.explained_variance_ratio_ )
+for i in range(0,4):
+    sheet_name='c%d'%i
+    tb= pd.read_excel(tb_path ,sheet_name)
+    x=tb.iloc[:,2:8].values
+    # print(x[:3])
+    my_pca=PCA(n_components='mle',svd_solver='full')
+    my_pca.fit(x)
+    print('The PCA for cluster %d'%i)
+    print(my_pca.components_)
+    print(my_pca.explained_variance_ratio_ )
