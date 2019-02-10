5f8a2f3264ac9a3177d0034618c0c1a27b7b9487
beiowolf007 <beiowolf007@163.com>
2019-02-10 12:07:37 +0800

    v0.28

--- autocode_3d.py
+++ autocode_3d.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
-import sklearn.preprocessing as pp
-from sklearn.cluster import AgglomerativeClustering
+from keras import regularizers
+from sklearn.cluster import KMeans
 
 def make_list(data,label):
     list0 = list()
     list1 = list()
     list2 = list()
     list3 = list()
+    list4=list()
+    list5=list()
     for i in range(len(data)):
         if label[i]==0:
             templist=[data[i,0],data[i,1],data[i,2],i,0]
@@ -25,22 +27,29 @@ def make_list(data,label):
         elif label[i]==3:
             templist = [data[i, 0], data[i, 1],data[i,2],i,3]
             list3.append(templist)
+        elif label[i]==4:
+            templist = [data[i, 0], data[i, 1],data[i,2],i,4]
+            list3.append(templist)
+        elif label[i]==5:
+            templist = [data[i, 0], data[i, 1],data[i,2],i,5]
+            list3.append(templist)
     array0=np.array(list0)
     array1=np.array(list1)
     array2 = np.array(list2)
     array3 = np.array(list3)
+    # array4=np.array(list4)
     return array0,array1,array2,array3
     
 
 #prepare data
-tb_path=r"F:\chenyw\毕业论文\毕业\data\gz_con.xlsx"
-tb= pd.read_excel(tb_path ,'Sheet1')
+tb_path=r"F:\work\毕业论文\毕业\data\gz_cx_con.xlsx"
+tb= pd.read_excel(tb_path ,'Sheet2')
 tb2=tb.iloc[:,2:8]
 # tb2=tb2.apply(lambda x:(x-np.min(x))/(np.max(x)-np.min(x)))
 # x=tb2.iloc[:,:].values
-t=tb2.iloc[:,:].values
+x=tb2.iloc[:,:].values
 # x=pp.minmax_scale(t)
-x=pp.normalize(t,axis=0)
+# x=pp.normalize(t,axis=0)
 print(tb2.shape)
 print(tb2.head())
 print(x[:4])
@@ -49,16 +58,16 @@ print(x[:4])
 
 #make encoder
 input_data=Input(shape=(6,))
-encoded=Dense(128,activation='relu')(input_data)
-encoded=Dense(64,activation='relu')(encoded)
-encoded=Dense(10,activation='relu')(encoded)
+encoded=Dense(128,activation='relu',activity_regularizer=regularizers.l1(10e-5))(input_data)
+encoded=Dense(64,activation='relu',activity_regularizer=regularizers.l1(10e-5))(encoded)
+encoded=Dense(10,activation='relu',activity_regularizer=regularizers.l1(10e-5))(encoded)
 encoded_data=Dense(3)(encoded)
 
 #make decoder
 decoded=Dense(10,activation='relu')(encoded_data)
 decoded=Dense(64,activation='relu')(decoded)
 decoded=Dense(128,activation='relu')(decoded)
-decoded_data=Dense(6,activation='tanh')(decoded)
+decoded_data=Dense(6,activation='linear')(decoded)
 
 #make model
 autoencoder=Model(inputs=input_data,outputs=decoded_data)
@@ -77,18 +86,21 @@ end_data=autoencoder.predict(x)
 # print(cluster_data[0:3,])
 
 #cluster and show up
-plot_num=221
 mark_dic={0:'o',1:'v',2:'*',3:'p'}
 
 
 fig=plt.figure()
 ax1=Axes3D(fig)
-cluster_alg=AgglomerativeClustering(n_clusters=4,linkage='ward')
+cluster_alg=KMeans(n_clusters=4,n_jobs=-1)
 clusted=cluster_alg.fit(cluster_data)
-ax1.scatter3D(cluster_data[:,0],cluster_data[:,1],cluster_data[:,2],c=clusted.labels_)
+# ax1.scatter3D(cluster_data[:,0],cluster_data[:,1],cluster_data[:,2],c=clusted.labels_)
 list0,list1,list2,list3=make_list(cluster_data,clusted.labels_)
+ax1.scatter3D(list0[:, 0], list0[:, 1],list0[:,2],marker='$A$', c='b')
+ax1.scatter3D(list1[:, 0], list1[:, 1],list1[:,2],marker='$B$', c='g')
+ax1.scatter3D(list2[:, 0], list2[:, 1],list2[:,2],marker='$C$', c='r')
+ax1.scatter3D(list3[:, 0], list3[:, 1],list3[:,2],marker='$D$', c='c')
 
-outpath=r'F:\chenyw\毕业论文\毕业\data\clust\gz_clust3d.xlsx'
+outpath=r'F:\work\毕业论文\毕业\data\clust\gz_clust3d_2.xlsx'
 outlist=np.ones([1,5])
 outlist=np.row_stack([list0,list1,list2,list3])
 outTable=pd.DataFrame(outlist,columns=['X','Y','Z','序号','分类'])
--- autocode_5times.py
+++ autocode_5times.py
@@ -3,40 +3,44 @@ from keras.models import Model
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing
-from sklearn.cluster import AgglomerativeClustering
-import openpyxl
+from sklearn.cluster import KMeans
+from keras import regularizers
 
-def make_list(data,label,phase):
+def make_list(data,label):
     list0 = list()
     list1 = list()
     list2 = list()
     list3 = list()
+    list4=list()
     for i in range(len(data)):
         if label[i]==0:
-            templist=[data[i,0],data[i,1],i,0,phase]
+            templist=[data[i,0],data[i,1],i,0]
             list0.append(templist)
         elif label[i]==1:
-            templist = [data[i, 0], data[i, 1],i,1,phase]
+            templist = [data[i, 0], data[i, 1],i,1]
             list1.append(templist)
         elif label[i]==2:
-            templist = [data[i, 0], data[i, 1],i,2,phase]
+            templist = [data[i, 0], data[i, 1],i,2]
             list2.append(templist)
         elif label[i]==3:
-            templist = [data[i, 0], data[i, 1],i,3,phase]
+            templist = [data[i, 0], data[i, 1],i,3]
             list3.append(templist)
+        elif label[i] ==4:
+            templist = [data[i, 0], data[i, 1], i,4]
+            list4.append(templist)
     array0=np.array(list0)
     array1=np.array(list1)
     array2 = np.array(list2)
     array3 = np.array(list3)
+    # array4=np.array(list4)
     return array0,array1,array2,array3
     
 
 #prepare data
-tb_path=r"F:\chenyw\毕业论文\毕业\data\gz_con.xlsx"
-tb= pd.read_excel(tb_path ,'Sheet1')
-tb2=tb.iloc[:,2:]
-tb2=tb2.apply(lambda x:(x-np.min(x))/(np.max(x)-np.min(x)))
+tb_path=r"F:\work\毕业论文\毕业\data\gz_cx_con.xlsx"
+tb= pd.read_excel(tb_path ,'ad')
+tb2=tb.iloc[:,2:8]
+# tb2=tb2.apply(lambda x:(x-np.min(x))/(np.max(x)-np.min(x)))
 x=tb2.iloc[:,:].values
 print(tb2.head())
 print(x[:4])
@@ -44,16 +48,17 @@ print(x[:4])
 
 #make encoder
 input_data=Input(shape=(6,))
-encoded=Dense(128,activation='relu')(input_data)
-encoded=Dense(64,activation='relu')(encoded)
-encoded=Dense(10,activation='relu')(encoded)
+encoded=Dense(128,activation='relu',activity_regularizer=regularizers.l1(10e-5))(input_data)
+encoded=Dense(64,activation='relu',activity_regularizer=regularizers.l1(10e-5))(encoded)
+encoded=Dense(10,activation='relu',activity_regularizer=regularizers.l1(10e-5))(encoded)
 encoded_data=Dense(2)(encoded)
+# encoded_data=Dense(2,activity_regularizer=regularizers.l1(10e-5))(encoded)
 
 #make decoder
 decoded=Dense(10,activation='relu')(encoded_data)
 decoded=Dense(64,activation='relu')(decoded)
 decoded=Dense(128,activation='relu')(decoded)
-decoded_data=Dense(6,activation='tanh')(decoded)
+decoded_data=Dense(6,activation='linear')(decoded)
 
 #make model
 autoencoder=Model(inputs=input_data,outputs=decoded_data)
@@ -75,25 +80,24 @@ autoencoder.compile(optimizer='Adam',loss='mse')
 plot_num=331
 mark_dic={0:'o',1:'v',2:'*',3:'P'}
 # outlist_f=np.ones([1,5])
-for i in range(0,9):
-    autoencoder.fit(x, x, batch_size=15, epochs=70, shuffle=True)
-    cluster_data = encoder_Model.predict(x)
-    end_data = autoencoder.predict(x)
-    cluster_alg = AgglomerativeClustering(n_clusters=4, linkage='ward')
-    clusted = cluster_alg.fit(cluster_data)
-    list0, list1, list2, list3 = make_list(cluster_data, clusted.labels_,i)
-    plt.subplot(plot_num)
-    plt.scatter(list0[:, 0], list0[:, 1], marker=mark_dic[0])
-    plt.scatter(list1[:, 0], list1[:, 1], marker=mark_dic[1])
-    plt.scatter(list2[:, 0], list2[:, 1], marker=mark_dic[2])
-    plt.scatter(list3[:, 0], list3[:, 1], marker=mark_dic[3])
-    plt.title("ward clust  %d times" % i)
-    plot_num = plot_num + 1
-    outpath=r'F:\chenyw\毕业论文\毕业\data\clust\gz_clust%d.xlsx'%i
-    outlist=np.row_stack([list0,list1,list2,list3])
-    # outlist_f=np.row_stack([outlist_f,outlist])
-    outdf=pd.DataFrame(outlist,columns=['x','y','序号','分类','运行次数'])
-    outdf.to_excel(outpath,sheet_name='clust')
+autoencoder.fit(x, x, batch_size=15, epochs=2, shuffle=True)
+cluster_data = encoder_Model.predict(x)
+end_data = autoencoder.predict(x)
+cluster_alg = KMeans(n_clusters=4,n_jobs=-1)
+clusted = cluster_alg.fit(cluster_data)
+list0, list1, list2,list3= make_list(cluster_data, clusted.labels_)
+# plt.scatter(cluster_data[:,0],cluster_data[:,1],c=clusted.labels_)
+# plt.subplot(plot_num)
+plt.scatter(list0[:, 0], list0[:, 1],marker='$A$', c=clusted.labels_)
+plt.scatter(list1[:, 0], list1[:, 1],marker='$B$', c=clusted.labels_)
+plt.scatter(list2[:, 0], list2[:, 1],marker='$C$', c=clusted.labels_)
+plt.scatter(list3[:, 0], list3[:, 1],marker='$D$', c=clusted.labels_)
+# plt.scatter(list4[:, 0], list4[:, 1], c=clusted.labels_)
+outpath=r'F:\work\毕业论文\毕业\data\clust\gz_clust.xlsx'
+outlist=np.row_stack([list0,list1,list2,list3])
+# outlist_f=np.row_stack([outlist_f,outlist])
+outdf=pd.DataFrame(outlist,columns=['x','y','序号','分类'])
+outdf.to_excel(outpath,sheet_name='clust')
 # outpath=r'F:\chenyw\毕业论文\毕业\data\clust\gz_con_clust.xlsx'
 
 
